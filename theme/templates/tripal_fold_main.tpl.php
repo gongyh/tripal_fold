a1d4c6b54ba78ef6ffbb18db14507b3964898d9e
zhousq <13854291622@163.com>
2021-11-11 16:56:34 +0800

    add tripal_fold.theme.inc

--- theme/templates/tripal_fold_main.tpl.php
+++ theme/templates/tripal_fold_main.tpl.php
@@ -1,6 +1,1 @@
-<?php
-$feature_id = $variables['node']->feature->feature_id;
-$sql = "SELECT pdb_loc FROM {tripal_fold} WHERE	feature_id = :feature_id";
-$args = array(':feature_id' => $feature_id);
-$result = db_query($sql, $args);
 
